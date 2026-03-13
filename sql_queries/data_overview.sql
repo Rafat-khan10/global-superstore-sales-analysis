317e7c4107506d629973c77fb1bc1e5a4bacf415
Rafat-khan10 <rafatkhan2210@gmail.com>
2026-03-13 23:15:49 +0600

    Update data_overview.sql

--- sql_queries/data_overview.sql
+++ sql_queries/data_overview.sql
@@ -1,1 +1,9 @@
+1.What is the overall dataset summary (customers, orders, revenue)?
 
+SELECT 
+       COUNT(DISTINCT customer_id) AS total_customers,
+       COUNT(DISTINCT order_id) AS total_orders,
+       ROUND(SUM(quantity * sales)::NUMERIC, 2) AS total_revenue,
+       ROUND(SUM(profit), 2) AS total_revenue,
+       COUNT(DISTINCT category) AS total_categories
+FROM orders;
